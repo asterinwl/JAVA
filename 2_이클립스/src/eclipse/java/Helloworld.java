be4a184dd675a09e31921d8fb5a340640c392ab2
asterinwl <85321878+asterinwl@users.noreply.github.com>
2021-08-31 23:47:37 +0900

    Add files via upload

---
+++ 2_이클립스/src/eclipse/java/Helloworld.java
@@ -0,0 +1,10 @@
+package eclipse.java;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ 3_숫자와 문자열/src/number/string/java/Number.java
@@ -0,0 +1,10 @@
+package number.string.java;
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println(8+2);
+		System.out.println(6/2);
+	}
+
+}
